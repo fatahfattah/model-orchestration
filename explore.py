6c0ea18335ffd773c55527fab45dd24f6659dbbd
fatahfattah <fatahfattah@hotmail.com>
2021-06-05 17:14:33 +0200

    Progress explore

--- explore.py
+++ explore.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 
 from dataloader import load_input
 from models.cnc import CNC_net
+from models.cncmany import CNCMANY_net
 from models.hl import HL_net
 from models.pc_experimental import PC_net
 """
@@ -37,13 +38,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     root_dir = args.root_dir if args.root_dir else ""
-    n = int(args.n) if args.n else 10
+    n = int(args.n) if args.n else float('inf')
 
     print(f"We will start model exploration...\nDataset directory: {root_dir}")
 
     print(f"Now we load in our nn's")
     model_mapping = {
                      "cnc": CNC_net(),
+                    #  "cncmany": CNCMANY_net(),
                      "hl": HL_net(),
                      "pc": PC_net()
                      }
@@ -55,6 +57,7 @@ if __name__ == "__main__":
 
         for image in random.sample(os.listdir(directory), min(n, len(os.listdir(directory)))):
             image_path = os.path.join(directory, image)
+            print(image_path)
 
             # Initialize our inputs dictionary and process the paths into data tensors
             inputs_dict = {"image": image_path}
@@ -62,12 +65,12 @@ if __name__ == "__main__":
 
             for name, agent in model_mapping.items():
                 inferences.setdefault(name, {})
-                inferences[name].setdefault(truth_label, [])
-                inferences[name][truth_label].append(agent.infer(inputs_tensor_dict[agent.input_type]))
+                inferences[name].setdefault(truth_label, []).append(agent.infer(inputs_tensor_dict[agent.input_type]))
     
 
     fig = plt.figure()
     fig_index = 1
+    filters = {}
     for name, agent in model_mapping.items():
         agent_inferences = inferences[name]
         classes = agent.classes
@@ -82,8 +85,11 @@ if __name__ == "__main__":
         for i, row in enumerate(mat):
             for j, cell in enumerate(row):
                 if cell:
-                    matrelative[i][j] = round(cell/matsums[i]*100, 2)
-                    
+                    matrelative[i][j] = round(cell/matsums[i], 2)
+                    if matrelative[i][j] >= 0.1:
+                        filters.setdefault(name, {})
+                        filters[name].setdefault(y_headers[i], []).append(classes[j])
+
         ax = fig.add_subplot(len(model_mapping.keys()), 1, fig_index)
         ax.set_title(agent.name)
         sns.heatmap(matrelative, annot=True)
@@ -91,5 +97,6 @@ if __name__ == "__main__":
         ax.set_xticklabels(classes, rotation = -45)
         fig_index += 1
     
+    print(f"We have found the following possible filters: {filters}")
     fig.tight_layout()
     plt.show()
