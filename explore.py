6318d57449420d7131712d02f422a1cd0e67ff90
fatahfattah <fatahfattah@hotmail.com>
2021-06-10 20:20:41 +0200

    progress explore

--- explore.py
+++ explore.py
@@ -83,6 +83,8 @@ if __name__ == "__main__":
     fig = plt.figure()
     fig_index = 1
     filters = {}
+    rankings = []
+
     for name, agent in model_mapping.items():
         if not name in inferences:
             continue
@@ -90,8 +92,20 @@ if __name__ == "__main__":
         agent_inferences = inferences[name]
         classes = agent.classes
         y_headers = list(agent_inferences.keys())
-        mat = matrelative = [[0 for i in range(len(classes))] for i in range(len(y_headers))]
+        mat = [[0 for i in range(len(classes))] for i in range(len(y_headers))]
+        matrelative = [[0 for i in range(len(classes))] for i in range(len(y_headers))]
         
+        """
+        agent:drawing -> {'(correct) nonchemical': ['not_drawing', 'not_not_drawing'], '(wrong) nonchemical': ['not_drawing'], 
+                        '(correct) manychemical': ['not_drawing', 'not_not_drawing'], '(wrong) manychemical': ['not_drawing', 'not_not_drawing'], 
+                        '(wrong) onechemical': ['not_drawing', 'not_not_drawing'], '(correct) onechemical': ['not_drawing', 'not_not_drawing']}
+
+        agent:not_drawing -> {'(correct) nonchemical': ['drawing', 'not_drawing'], '(wrong) nonchemical': ['not_drawing'], 
+                            '(correct) manychemical': ['drawing'], '(wrong) manychemical': ['drawing'], 
+                            '(wrong) onechemical': ['drawing'], '(correct) onechemical': ['drawing', 'not_drawing']}
+        """
+
+        # Register the amount of hits for each true_label (correct/wrong) combination with ancillery output
         for i, label in enumerate(y_headers):
             for pred in agent_inferences[label]:
                 mat[i][classes.index(pred)] += 1
@@ -104,6 +118,25 @@ if __name__ == "__main__":
                     if matrelative[i][j] >= 0.1:
                         filters.setdefault(name, {})
                         filters[name].setdefault(y_headers[i], []).append(classes[j])
+        
+        def max_wrong_correct(wrong: int, correct: int) -> float:
+            return wrong-correct
+
+
+        def min_rank(rank_wrong:int, rank_correct: int) -> float:
+            return min(rank_wrong//rank_correct)
+
+        for i, c in enumerate(classes):
+            for t in truth_labels:
+                wrong_i = y_headers.index(f"(wrong) {t}") if f"(wrong) {t}" in y_headers else None
+                correct_i = y_headers.index(f"(correct) {t}") if f"(correct) {t}" in y_headers else None
+                if wrong_i and correct_i:
+                    print(f"{t}->")
+                    wrong_ratio = matrelative[wrong_i][i]
+                    correct_ratio = matrelative[correct_i][i]
+                    max_wrong_correct_rank = max_wrong_correct(wrong_ratio, correct_ratio)
+                    rankings.append([t, max_wrong_correct])
+                    print(f"\twrong ratio: {wrong_ratio}, correct ratio: {correct_ratio}, rank (max_wrong_correct): {max_wrong_correct_rank}")
 
         ax = fig.add_subplot(len(model_mapping.keys()), 1, fig_index)
         ax.set_title(agent.name)
